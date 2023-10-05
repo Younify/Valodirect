55e6554875c33e69a9c03a0832d5560c29abd301
Marco Silva <marcoarsilva3@gmail.com>
2023-10-05 12:09:41 +0200

    Change the Pdf Document on the Accounting to tex-end all the Table Col and Text Font Smaller on the Table Lines, Also add a Logo on it

--- valo_vendor_bill_approval/__manifest__.py
+++ valo_vendor_bill_approval/__manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': "Valo Vendor bill approval",
-    'author': "REVO TECH",
+    'name': "ValoDirect Vendor Bill Approval",
+    'author': "REVO TECH & Younify",
     'summary': "",
     'description': "",
-    'qweb': [],
-    'demo': [],
-    'depends': ['account','analytic','account_3way_match'],
+    'category': "Account",
+    'depends': ['account', 'analytic', 'account_3way_match'],
     'data': [
         'security/account_security.xml',
         'security/ir.model.access.csv',
         'views/account_analytic_line.xml',
-        'views/account_move_line.xml',
         'views/account_move.xml',
-        'views/res_config_settings_views.xml',
+        'views/account_move_line.xml',
         'views/product_template.xml',
+        'views/res_config_settings_views.xml',
+        'views/report_saleorder_document.xml',
         'wizards/account_move_approval_wizard.xml'
-        ],
+    ],
     'installable': True,
     'application': True,
     'license': 'OEEL-1',
--- valo_vendor_bill_approval/wizards/account_move_approval_wizard.py
+++ valo_vendor_bill_approval/wizards/account_move_approval_wizard.py
@@ -1,36 +1,36 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
+
 
 class AccountMoveApprovalWizard(models.TransientModel):
     _name = 'account.move.approval.wizard'
     _description = 'Account Move Approval Wizard'
 
-    move_id = fields.Many2one('account.move',required=True, readonly=True)
+    move_id = fields.Many2one('account.move', required=True, readonly=True)
     parnter_id = fields.Many2one('res.partner', related='move_id.partner_id')
     user_id = fields.Many2one('res.users', required=True, readonly=True)
 
     message = fields.Html('Comment')
-    line_ids = fields.One2many(comodel_name='account.move.approval.wizard.line',inverse_name='wizard_id')
+    line_ids = fields.One2many(comodel_name='account.move.approval.wizard.line', inverse_name='wizard_id')
 
-    def create(self,values):
+    def create(self, values):
         res = super().create(values)
         for wizard in res:
             wizard._compute_account_move_line_ids()
         return res
-
 
     def _compute_account_move_line_ids(self):
         self.ensure_one()
 
         my_lines = self.move_id.invoice_line_ids.filtered(lambda l: self.user_id.id in l.approver_user_id.ids)
 
-        if self.env.user.has_group('valo_vendor_bill_approval.group_account_approver') :
+        if self.env.user.has_group('valo_vendor_bill_approval.group_account_approver'):
             lines = self.move_id.invoice_line_ids
         else:
             lines = my_lines
 
         self.line_ids.unlink()
         for line in lines:
-            vals ={
+            vals = {
                 'wizard_id': self.id,
                 'move_line_id': line.id,
                 'include_in_approval_selection': True if my_lines.filtered(lambda l: l.id == line.id) else False
@@ -43,12 +43,12 @@ class AccountMoveApprovalWizard(models.TransientModel):
     def decline(self):
         self._approve_lines(False)
 
-    def _approve_lines(self,result):
+    def _approve_lines(self, result):
         if self.message:
             self.move_id.message_post(body=self.message)
 
         selected_lines = self.line_ids.filtered(lambda l: l.include_in_approval_selection == True)
-        self.move_id.approve_lines(result,selected_lines.move_line_id)
+        self.move_id.approve_lines(result, selected_lines.move_line_id)
 
 
 class AccountMoveApprovalWizardLine(models.TransientModel):
@@ -69,4 +69,3 @@ class AccountMoveApprovalWizardLine(models.TransientModel):
     price_total = fields.Monetary(related='move_line_id.price_total')
     approver_user_id = fields.Many2many(comodel_name='res.users', related='move_line_id.approver_user_id')
     currency_id = fields.Many2one(comodel_name='res.currency', related='move_line_id.currency_id')
-    
