78c78653316879404fdc6dd660df152480f994e2
Sander V <sv@revotech.be>
2023-11-06 22:55:01 -0800

    fix email from account followup approval

---
+++ valo_account_followup_email/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Valo Account follow up",
+    'author': "REVO TECH",
+    'summary': "",
+    'description': "",
+    'qweb': [],
+    'demo': [],
+    'depends': ['account_followup'],
+    'data': [
+        ],
+    'installable': True,
+    'application': True,
+    'license': 'OEEL-1',
+}
